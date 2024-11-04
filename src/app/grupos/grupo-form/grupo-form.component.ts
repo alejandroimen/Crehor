20d7bce8df71450aa019b23ad57328241a824d9c
alejandroimen <aimr2910@gmail.com>
2024-11-04 01:05:02 -0600

    Correccion de errores

--- src/app/grupos/grupo-form/grupo-form.component.ts
+++ src/app/grupos/grupo-form/grupo-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
+import { GruposService } from '../services/grupos.service';
+import { Igrupo } from '../interfaces/igrupo';
 @Component({
   selector: 'app-grupo-form',
   templateUrl: './grupo-form.component.html',
@@ -8,8 +9,31 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class GrupoFormComponent {
   @Input() visible: boolean = false
   @Output() visibleChange = new EventEmitter<boolean>()
+  grupo: Igrupo = {
+    grado: 0,
+    grp: '',
+    horarioAsignado: false
+  }
+
+  constructor(private grupoService: GruposService){}
 
   changeVisibility(): void {
     this.visibleChange.emit(!this.visible)
+  }
+
+  addGrupo(){
+    let grupoAux: Igrupo = {
+      grado: this.grupo.grado,
+      grp: this.grupo.grp,
+      horarioAsignado: this.grupo.horarioAsignado
+    }
+    if(this.grupo.grado && this.grupo.grp){
+      this.grupoService.addGrupo(grupoAux)
+      console.log(localStorage.getItem('listaGrupos'));
+      this.visibleChange.emit(!this.visible)
+    }
+    else {
+      alert('Completa todos los campos xfi u.u')
+    }
   }
 }
